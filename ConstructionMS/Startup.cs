d3c2f07d4cf6cdf4579cb6342bda95dbd2fce5ec
hungdang45 <46567162+hungdang45@users.noreply.github.com>
2019-10-01 23:40:58 +0700

    Add project files.

---
+++ ConstructionMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConstructionMS.Startup))]
+namespace ConstructionMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
